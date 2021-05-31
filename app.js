98445dba26e85261dbe9fd05d83ee7ce80b11b5d
Waleed-Asim <83092895+Waleed-Asim@users.noreply.github.com>
2021-06-01 02:30:02 +0500

    Add initial files

---
+++ app.js
@@ -0,0 +1,50 @@
+// Q1
+
+// var user = prompt("Enter +ve integar")
+
+// console.log((user));
+// console.log(Math.round(user));
+// console.log(Math.floor(user));
+// console.log(Math.ceil(user));
+
+// Q2
+
+// var user = prompt("Enter -ve integar")
+
+// console.log((user));
+// console.log(Math.round(user));
+// console.log(Math.floor(user));
+// console.log(Math.ceil(user));
+
+// Q4
+
+// var dice = Math.floor( Math.random() * 6 ) +1;
+// alert('You number is ' + dice);
+
+// Q6   
+
+// var randomNum = Math.random() * 100;
+// console.log("Random num between 1 & 100 is: " + Math.round(randomNum));
+
+// Q7
+
+// var user = prompt("Enter your weight in kilograms")
+
+// alert("Yor weight is: " + parseInt(user) + "kgs");
+
+// Q8
+
+// var secretNum = 5;
+
+// var user = +prompt("Enter a number between 1 and 10");
+
+// if(user === secretNum){
+//     alert("Congratulations!");
+// }
+
+// else{
+//     alert("Try again!");
+// }
+
+
+
